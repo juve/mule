29dcac664c3f2fbc9009978e949ae467280aa521
Gideon Juve <juve@usc.edu>
2010-04-12 21:35:08 +0000

    Fix deadlock issues (hopefully)
    git-svn-id: https://juve.isi.edu/svn/juve/mule@799 7cf33618-8928-0410-9edc-aa4ff926e27f

---
+++ Makefile
@@ -0,0 +1,14 @@
+BSDDB3=bsddb3-5.0.0.tar.gz
+
+all: lib/bsddb3
+
+lib/bsddb3:
+	cd externals; \
+	tar xzvf $(BSDDB3).tar.gz; \
+	cd $(BSDDB3); \
+	python setup.py build; \
+	mv build/lib.*/bsddb3 ../../lib
+
+clean:
+	rm -rf lib/bsddb3 externals/$(BSDDB3)
+
--- lib/mule/bdb.py
+++ lib/mule/bdb.py
@@ -29,26 +29,34 @@ if version < (4,7):
 	raise ImportError('bsddb version 4.7 or later required')
 del version
 
-def with_transaction(method):
+def with_transaction(method, retries=3):
 	def with_transaction(self, *args, **kwargs):
-		if len(args)>0 and isinstance(args[0],Database):
-			return method(self, *args, **kwargs)
-		else:
+		deadlocks = 0
+		while True:
 			txn = self.env.txn_begin()
 			try:
 				result = method(self, txn, *args, **kwargs)
 				txn.commit()
 				return result
+			except DBLockDeadlockError, e:
+				txn.abort()
+				deadlocks += 1
+				if deadlocks < retries:
+					self.log.info("Deadlock detected, retrying")
+					continue
+				else:
+					self.log.error("Deadlock detected, aborting")
+					raise
 			except:
 				txn.abort()
 				raise
 	return with_transaction
 
-class CheckpointThread(Thread):
+class DatabaseManagerThread(Thread):
 	def __init__(self, env, interval=300):
 		Thread.__init__(self)
 		self.setDaemon(True)
-		self.log = log.get_log("ckpt_thread")
+		self.log = log.get_log("bdb manager")
 		self.env = env
 		self.interval = interval
 	
@@ -58,6 +66,9 @@ class CheckpointThread(Thread):
 				time.sleep(self.interval)
 				self.log.info("checkpointing database")
 				self.env.txn_checkpoint(0,0)
+				if not hasattr(self.env, "log_set_config"):
+					self.log.info("archiving logs")
+					self.env.log_archive(bdb.DB_ARCH_REMOVE)
 			except:
 				t, e, tb = sys.exc_info()
 				self.log.exception(e)	
@@ -77,8 +88,9 @@ class Database(object):
 		self.env.set_lk_max_lockers(self.max_txns*2)
 		self.env.set_lk_max_locks(self.max_txns*2)
 		self.env.set_lk_max_objects(self.max_txns*2)
+		self.env.set_lk_detect(bdb.DB_LOCK_DEFAULT)
 		self.env.set_flags(bdb.DB_TXN_NOSYNC, True)
-		if bdb.version() > (4,7):
+		if hasattr(self.env, "log_set_config"):
 			self.env.log_set_config(bdb.DB_LOG_AUTO_REMOVE, True)
 		self.env.open(self.path, bdb.DB_CREATE | bdb.DB_INIT_LOCK | 
 				bdb.DB_INIT_LOG | bdb.DB_INIT_MPOOL | bdb.DB_INIT_TXN | 
@@ -93,6 +105,9 @@ class Database(object):
 			txn.commit()
 		else:
 			self.db.open(self.dbpath, name, flags=bdb.DB_CREATE|bdb.DB_THREAD, dbtype=bdb.DB_BTREE)
+			
+		self.thread = DatabaseManagerThread(self.env)
+		self.thread.start()
 	
 	def close(self):
 		self.env.log_flush()
@@ -126,9 +141,10 @@ class RLSDatabase(Database):
 					cur.delete()
 		finally:
 			cur.close()
-			
-	def lookup(self, lfn):
-		cur = self.db.cursor()
+	
+	@with_transaction
+	def lookup(self, txn, lfn):
+		cur = self.db.cursor(txn)
 		try:
 			result = []
 			current = cur.set(lfn)
@@ -146,8 +162,9 @@ class CacheDatabase(Database):
 		path = os.path.join(home, "var", "cache")
 		Database.__init__(self, path, "cache", duplicates=False)
 	
-	def get(self, lfn):
-		current = self.db.get(lfn)
+	@with_transaction
+	def get(self, txn, lfn):
+		current = self.db.get(lfn, txn)
 		if current is not None:
 			return pickle.loads(current)
 		else:
@@ -161,9 +178,10 @@ class CacheDatabase(Database):
 	@with_transaction
 	def remove(self, txn, lfn):
 		self.db.delete(lfn, txn)
-		
+	
+	@with_transaction
 	def list(self):
-		cur = self.db.cursor()
+		cur = self.db.cursor(txn)
 		try:
 			result = []
 			current = cur.first()
