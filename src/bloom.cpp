71e24ed9081e725c3d15fe9b6764a0519f5f4473
Gideon Juve <juve@usc.edu>
2010-05-07 19:16:14 +0000

    Allow multiple filters
    git-svn-id: https://juve.isi.edu/svn/juve/mule@825 7cf33618-8928-0410-9edc-aa4ff926e27f

--- src/bloom.cpp
+++ src/bloom.cpp
@@ -26,7 +26,7 @@ static int b64_value(unsigned char c)
 static char * b64_decode(const char *s, int *n)
 {
 	int s_length = strlen(s);
-	if (s_length == 0) return new char[0];
+	if (s_length == 0) return NULL;
 	int i = 0;
 	for(int j = s_length - 1; j > 0 && s[j] == '='; j--) {
 		i++;
@@ -34,8 +34,7 @@ static char * b64_decode(const char *s, int *n)
 	 
 	int k = (s_length * 6) / 8 - i;
 	*n = k;
-	char *result = new char[k+1];
-	result[k] = '\0';
+	char *result = new char[k];
 	int l = 0;
 	for(int i1 = 0; i1 < s_length; i1 += 4) {
 		int j1 = (b64_value(s[i1])     << 18) + 
@@ -68,40 +67,56 @@ void bloom_compare(const int number_of_arguments,
 				   const ClassAdSharedValue *arguments,
 				   ClassAdSharedValue *result)
 {
-	int count;
-	
-	if (number_of_arguments != 2 || 
-		arguments[0].type != ClassAdSharedType_String ||
-		arguments[1].type != ClassAdSharedType_String) {
-		fprintf(stderr, "ERROR: invalid arguments\n");
+	int count = 0;
+
+	if (number_of_arguments % 2 != 0) { 
+		fprintf(stderr, "ERROR: invalid arguments: must be even number\n");
 		result->type = ClassAdSharedType_Error;
 		return;
 	}
 
-	int n1, n2;
-	char *r1 = b64_decode(arguments[0].text, &n1);
-	char *r2 = b64_decode(arguments[1].text, &n2);
-	
-	if (n1 != n2) {
-		fprintf(stderr, "ERROR: bloom filters are different lengths\n");
-		result->type = ClassAdSharedType_Error;
-		goto exit;
-	}
+	int i;
+	for (i=0; i<number_of_arguments; i+=2) {
+		if (arguments[i].type != ClassAdSharedType_String ||
+			arguments[i+1].type != ClassAdSharedType_String) {
+			fprintf(stderr, "ERROR: invalid argument: expected string\n");
+			result->type = ClassAdSharedType_Error;
+			return;
+		}
 
-	// AND the bits together
-	for (int i=0; i<n1; i++) {
-		r1[i] = r1[i] & r2[i];
-	}
+		int n1, n2;
+		char *r1 = b64_decode(arguments[i].text, &n1);
+		char *r2 = b64_decode(arguments[i+1].text, &n2);
 
-	// Count the # of bits in ANDed bitstring	
-	count = popcount(r1, n1);
+		if (r1 == NULL || r2 == NULL) {
+			fprintf(stderr, "ERROR: invalid argument\n");
+			result->type = ClassAdSharedType_Error;
+			if (r1) delete[] r1;
+			if (r2) delete[] r2;
+		}
+		
+		if (n1 != n2) {
+			fprintf(stderr, "ERROR: bloom filters are different lengths\n");
+			result->type = ClassAdSharedType_Error;
+			delete [] r1;
+			delete [] r2;
+			return;
+		}
+
+		// AND the bits together
+		for (int j=0; j<n1; j++) {
+			r1[j] = r1[j] & r2[j];
+		}
+
+		// Count the # of bits in ANDed bitstring	
+		count += popcount(r1, n1);
+
+		delete [] r1;
+		delete [] r2;
+	}
 	
 	result->type = ClassAdSharedType_Integer;
 	result->integer = count;
-
-exit:
-	delete[] r1;
-	delete[] r2;
 
 	return;
 }
